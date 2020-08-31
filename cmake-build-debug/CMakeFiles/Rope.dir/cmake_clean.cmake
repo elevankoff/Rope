c2de6fb7dbe72d7e9243b26317ed1fed81e68ade
elevankoff <yegor.levankov@yandex.com>
2020-08-31 07:42:54 +0300

    Initializing commit

---
+++ cmake-build-debug/CMakeFiles/Rope.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rope.dir/src/main.cpp.obj"
+  "CMakeFiles/Rope.dir/src/Rope.cpp.obj"
+  "CMakeFiles/Rope.dir/src/ImplicitTreap.cpp.obj"
+  "CMakeFiles/Rope.dir/src/ImplicitTreapNode.cpp.obj"
+  "Rope.pdb"
+  "Rope.exe"
+  "Rope.exe.manifest"
+  "libRope.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Rope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Rope.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Rope.
+# This may be replaced when dependencies are built.
